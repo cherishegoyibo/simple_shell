dee5eef8cc2d13b53744d579201c1eb4e7ea476a
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-09-19 03:24:15 -0700

    Remove comments and fix betty

--- _env.c
+++ _env.c
@@ -1,24 +1,6 @@
 #include "shell.h"
 
 /**
- * print_rev -  prints a string, in reverse.
- * @s: string to be printed.
- *
- * Return: Nothing
-*/
-
-void print_rev(char *s)
-{
-	int i = 0;
-
-	while (s[i])
-		i++;
-	while (i--)
-		_putchar(s[i]);
-	_putchar('\n');
-}
-
-/**
  * _env - prints environment variables.
  *
  * Return: Nothing.
@@ -26,24 +8,10 @@ void print_rev(char *s)
 void _env(void)
 {
 	int i = 0;
-	/*int env_count = 0;*/
 
 	while (environ[i])
-        {
-                
-                _puts(environ[i]);
-                i++;
-        }
-	/*while (environ[i])
 	{
-		
-		env_count++;
+		_puts(environ[i]);
 		i++;
 	}
-	while (environ[env_count - 1])
-        {
-
-                _puts(environ[env_count]);
-                env_count--;
-        }*/
 }
